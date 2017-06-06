14b9d103e799dbdf72888a4e0bdb8adf39fa48a1
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2017-06-06 14:33:32 +0200

    tunnel as used

--- apps/alexa-fb-handoff/lib/tunnel.js
+++ apps/alexa-fb-handoff/lib/tunnel.js
@@ -1,6 +1,6 @@
 const EventEmitter = require('events')
 const ngrok = require('ngrok')
-const config = require('./config')
+const config = require('../config')
 
 const ee = new EventEmitter()
 
@@ -8,7 +8,7 @@ ngrok.connect({
   proto: 'http', // http|tcp|tls
   addr: 3000, // port or network address
   // auth: 'user:pwd', // http basic authentication for tunnel
-  // subdomain: 'b9926e1e-c92b-442f-8532-a2cf45b26c99',
+  subdomain: 'happy-travel',
   authtoken: config.ngrok_token, // your authtoken from ngrok.com
   region: 'eu', // one of ngrok regions (us, eu, au, ap), defaults to us,
   configPath: './ngrok.yml' // custom path for ngrok config file
