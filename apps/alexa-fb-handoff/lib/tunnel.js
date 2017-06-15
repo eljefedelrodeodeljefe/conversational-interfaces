e0a5c03ee5298b9051c689ae529b4878a371f565
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2017-06-15 18:38:25 +0200

    export tunnel

--- apps/alexa-fb-handoff/lib/tunnel.js
+++ apps/alexa-fb-handoff/lib/tunnel.js
@@ -4,18 +4,24 @@ const config = require('../config')
 
 const ee = new EventEmitter()
 
-ngrok.connect({
-  proto: 'http', // http|tcp|tls
-  addr: 3000, // port or network address
-  // auth: 'user:pwd', // http basic authentication for tunnel
-  subdomain: 'happy-travel',
-  authtoken: config.ngrok_token, // your authtoken from ngrok.com
-  region: 'eu', // one of ngrok regions (us, eu, au, ap), defaults to us,
-  configPath: './ngrok.yml' // custom path for ngrok config file
-}, (err, url) => {
-  console.log(url)
-  if (err) {
-    throw err
-  }
-  ee.emit('ready')
-})
+exports.ee = ee
+
+exports.connect = (cb) => {
+  ngrok.connect({
+    proto: 'http', // http|tcp|tls
+    addr: 3000, // port or network address
+    // auth: 'user:pwd', // http basic authentication for tunnel
+    subdomain: 'happy-travel',
+    authtoken: config.ngrok_token, // your authtoken from ngrok.com
+    region: 'eu', // one of ngrok regions (us, eu, au, ap), defaults to us,
+    configPath: './ngrok.yml' // custom path for ngrok config file
+  }, (err, url) => {
+    console.log(url)
+    if (err) {
+      throw err
+    }
+
+    ee.emit('ready')
+    return cb(null)
+  })
+}
