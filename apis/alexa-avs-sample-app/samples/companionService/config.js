5fbd87160698a6fb5a502bb5f580258fe53c1a94
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2017-06-15 18:17:27 +0200

    add sample avs

---
+++ apis/alexa-avs-sample-app/samples/companionService/config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+/**
+ * @module
+ * This module defines the settings that need to be configured for a new
+ * environment.
+ * The clientId and clientSecret are provided when you create
+ * a new security profile in Login with Amazon.
+ *
+ * You will also need to specify
+ * the redirect url under allowed settings as the return url that LWA
+ * will call back to with the authorization code.  The authresponse endpoint
+ * is setup in app.js, and should not be changed.
+ *
+ * lwaRedirectHost and lwaApiHost are setup for login with Amazon, and you should
+ * not need to modify those elements.
+ */
+var config = {
+    clientId: '',
+    clientSecret: '',
+    redirectUrl: 'https://localhost:3000/authresponse',
+    lwaRedirectHost: 'amazon.com',
+    lwaApiHost: 'api.amazon.com',
+    validateCertChain: true,
+    sslKey: path.resolve(__dirname, '../javaclient/certs/server/node.key'),
+    sslCert: path.resolve(__dirname, '../javaclient/certs/server/node.crt'),
+    sslCaCert: path.resolve(__dirname, '../javaclient/certs/ca/ca.crt'),
+    products: {
+        "": ["123456"], // Fill in with valid device values, eg: "testdevice1": ["DSN1234", "DSN5678"]
+    },
+};
+
+module.exports = config;
